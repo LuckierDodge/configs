ac8a5c770e5888732d15ebcf7cbcbb532cf4b10a
Ryan Lewis <ryandlewis.rl@gmail.com>
2019-05-05 13:05:01 -0500

    Updated install script

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-set -e
-
 cd ~
-rm -rf  .bashrc .dircolors .gitconfig .profile .tmux.conf .vim .vimrc .zshrc
+
+mkdir .conf_backup
 
 for file in .aliases .bashrc .dircolors .gitconfig .profile .tmux.conf .vim/ .vimrc .zshrc; do
+	cp $file .conf_backup/$file
+	unlink $file
+	rm -rf $file
 	ln -s repos/configs/$file
 done
-
-. .bashrc
