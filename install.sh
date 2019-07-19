89e2a2142d9f59d2a57e5ca583daeba0f90bdaf0
Ryan Lewis <ryandlewis.rl@gmail.com>
2019-07-19 10:48:32 -0500

    Customizations for Shatterdome

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd ~
 mkdir .conf_backup
 
 for file in .aliases .bashrc .dircolors .gitconfig .profile .tmux.conf .vim/ .vimrc .zshrc; do
-	cp $file .conf_backup/$file
+	cp -r $file .conf_backup/$file
 	unlink $file
 	rm -rf $file
 	ln -s repos/configs/$file
